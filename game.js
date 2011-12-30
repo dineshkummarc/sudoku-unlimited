8580d5241f321a9c6c7ea05c589f587af3759696
Matt Campbell <matt@mattcampbell.net>
2011-12-30 14:26:05 -0500

    Setting up image preloader

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 (function () {
-	var generator, game, context;
+	var generator, game, images, context;
 
 	function createCanvas(width, height, node) {
 		var canvas = document.createElement('canvas');
@@ -49,14 +49,50 @@
 
 	context = createCanvas(800, 600, document.body);
 
-	game = {
-		captureKey: function (code) {
-			return true;
-		},
-		keyPressed: function (code) {},
-		keyReleased: function (code) {},
-		mouseWheel: function (delta) {
-			console.log(delta);
+	game = (function () {
+		function init() {
 		}
-	};
+
+		return ({
+			captureKey: function (code) {
+				return true;
+			},
+			keyPressed: function (code) {},
+			keyReleased: function (code) {},
+			mouseWheel: function (delta) {
+				console.log(delta);
+			},
+			init: init
+
+		});
+	}());
+
+	/* Preload images */
+	(function loadImages(callback) {
+		var files, count;
+		count = 0;
+		images = {};
+		files = [
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'about', 'button_over', 'button', 'exit', 'generating',
+			'hide_errors', 'hide', 'logo', 'new_puzzle', 'restart',
+			'show_errors', 'show_solution', 'x', 'youwin'
+		];
+		
+		files.forEach(function (file) {
+			var image = new Image();
+
+			image.onload = function () {
+				console.log(image.src + ' loaded');
+				images[file] = image;
+
+				count += 1;
+				if (count === files.length) {
+					callback();
+				}
+			};
+
+			image.src = 'images/' + file + '.png';
+		});
+	}(game.init));
 }());
